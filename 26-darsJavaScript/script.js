bf7342e9c87c1d19ef659d801cd7a067bcbdec2d
Farrukh Ganiboyev <farrukhechos@gmail.com>
2026-01-13 17:22:22 +0500

    I started learning javascript

---
+++ 26-darsJavaScript/script.js
@@ -0,0 +1,27 @@
+// console.log("Hello, World!");
+
+let a = 10
+let ism = "Farrux"
+let isAdmin = true
+
+const p1 = 3.14
+
+
+let person ={
+    name:"Vali",
+    surname: "valiyev",
+    age: 25,
+    isMarried: true
+}
+
+console.log(person.surname)
+console.log(person.name)
+
+
+let x = 20
+let y = 25
+if (x > y){
+    console.log("X katta Y dan")
+}else{
+    console.log("Y katta X dan")
+}
